ccd63ea5455474ead9d898932ee81830a4331866
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-01-05 11:56:46 -0800

    Added stylesheet, started thumbnail size chooser

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,7 +2,7 @@ module PhotoGroove exposing (main)
 
 import Array exposing (Array)
 import Browser
-import Html exposing (Html, button, div, h1, img, text)
+import Html exposing (Html, button, div, h1, h3, img, input, label, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
@@ -14,14 +14,15 @@ urlPrefix =
 
 update : Msg -> Model -> Model
 update msg model =
-    if msg.description == "ClickedPhoto" then
-        { model | selectedUrl = msg.data }
+    case msg.description of
+        "ClickedPhoto" ->
+            { model | selectedUrl = msg.data }
 
-    else if msg.description == "ClickedSurpriseMe" then
-        { model | selectedUrl = "3.jpeg" }
+        "ClickedSurpriseMe" ->
+            { model | selectedUrl = "3.jpeg" }
 
-    else
-        model
+        _ ->
+            model
 
 
 viewThumbnail : String -> Photo -> Html Msg
@@ -34,6 +35,33 @@ viewThumbnail selectedUrl thumb =
         []
 
 
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input [ type_ "radio", name "size" ] []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
 type alias Msg =
     { description : String, data : String }
 
@@ -43,7 +71,9 @@ view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , button [ onClick { description = "ClickedSurpriseMe", data = "" } ] [ text "Surprise Me!" ]
-        , div [ id "thumbnails" ]
+        , h3 [] [ text "Thumbnail Size:" ]
+        , div [ id "choose-size" ] (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map
                 (viewThumbnail model.selectedUrl)
                 model.photos
@@ -57,7 +87,10 @@ type alias Photo =
 
 
 type alias Model =
-    { photos : List Photo, selectedUrl : String }
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
 
 
 initialModel : Model
@@ -68,6 +101,7 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Large
     }
 
 
